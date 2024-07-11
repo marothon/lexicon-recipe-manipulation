d91073dcd29ddc56b3f8111f26a9240f98979387
marothon <marothon@gmail.com>
2024-07-11 15:15:40 +0200

    Solve tasks 7-13 of part 2

--- wrong.js
+++ wrong.js
@@ -22,24 +22,31 @@ clockIconTag.classList.add('material-icons');
 const durationTag = document.querySelector('.time-container span.time');
 durationTag.textContent = '60+ min';
 
-
 // 7. The src path to the image is wrong, or atleast it's showing the wrong image. Change it to the correct one. The available images can be found in the assets folder.
-
+const imgTag = document.querySelector('.image-container img');
+imgTag.setAttribute('src', 'assets/frozen-cheesecake-slice.jpg');
 
 // 8. The background color of the ingredients list container is wrong. Fix it.
+const ingredientsTag = document.querySelector('.ingredients-container');
+ingredientsTag.style.backgroundColor = 'rgb(249, 249, 249)';
 
-
-// 9. The ingredients are divided in to two parts, one for the bottom and one for the paste. In the list of the ingredients to the bottom, there is a text instead of two list items. Remove the text and add those two list items.
-
+// 9. The ingredients are divided in to two parts, one for the bottom and one for the paste.
+// In the list of the ingredients to the bottom, there is a text instead of two list items. Remove the text and add those two list items.
+const ingredientsCakeBottomTag = document.querySelector('.ingredients-list-bottom');
+ingredientsCakeBottomTag.innerHTML = '<li>15st digestivetex</li><li>Lite smör</li>';
 
 // 10. The third ingredient in the list of ingredients to the paste is wrong. Change that specific ingredient to the correct one.
-
+const ingredientsPasteTag = document.querySelector('.ingredients-list-paste');
+ingredientsPasteTag.children[2].innerHTML = '3tsk vaniljsocker';
 
 // 11. There is also a missing ingredient in the list of ingredients to the paste. Look and see what it is and add that one the the end of the list.
-
+ingredientsPasteTag.insertAdjacentHTML('beforeend', '<li>400g naturell philadelphiaost</li>');
 
 // 12. The text "Instructions" to the left, beneath the image, has some shadow styling applied to it. Remove that styling.
-
+const instructionsTitleTag = document.querySelector('.instructions-container h3');
+instructionsTitleTag.classList.remove('shadow');
 
 // 13. Two list elements of the list of instructions are incorrect. Find them and change them to the correct ones.
-
+const instructionsListTag = document.querySelector('.instructions-list');
+instructionsListTag.children[1].innerHTML = 'Separera ägggulor och äggvitor. Äggvitorna lägger du i en stor bunke, äggulorna i en liten bunke.';
+instructionsListTag.children[8].innerHTML = 'Ställ in i frysen över natten.';
