d39ad95ce505cd639c79cadcae6274eded260d1b
marothon <marothon@gmail.com>
2024-07-11 13:17:27 +0200

    Divide assignment part 1 & 2 into .js files and link them to the HTML-documents

---
+++ index.js
@@ -0,0 +1,28 @@
+// 1. What is the name of the recipe?
+
+
+// 2. What HTML tag is used to display the Recipe name?
+
+
+// 3. What is the font size of the paragraph tag with the class "description".
+
+
+// 4. What is the value of the alt atrribute on the image?
+
+
+// 5. What is the dimensions and the url of the image? Create an object that looks like this, and log it to the console:
+// {
+//   url: string
+//   height: number,
+//   width: number,
+// }
+
+
+// 6. How many ingredients has the paste?
+
+
+// 7. Which is the forth element in the list containing the ingredients for the paste?
+
+
+// 8. Create an an array of objects from the instructions. Each element in the array should be an object that looks like this:
+
